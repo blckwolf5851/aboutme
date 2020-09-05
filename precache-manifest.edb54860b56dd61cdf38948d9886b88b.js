3a6100e2b05e17821b5598e08a15ce5aa69ec749
Sherry Yuan <154757929sherry@gmail.com>
2020-09-05 14:12:50 -0400

    Updates

---
+++ precache-manifest.edb54860b56dd61cdf38948d9886b88b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70bf791dcc967ab31aa94c7fa59790ef",
+    "url": "./index.html"
+  },
+  {
+    "revision": "df208fb56efdef3fecf3",
+    "url": "./static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "12ffe5d5e28214366ee2",
+    "url": "./static/js/2.866e22eb.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "./static/js/2.866e22eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df208fb56efdef3fecf3",
+    "url": "./static/js/main.88b51f7a.chunk.js"
+  },
+  {
+    "revision": "8510b6252ae3f6c56072",
+    "url": "./static/js/runtime-main.e7d84236.js"
+  }
+]);
